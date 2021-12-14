2dd4dbdfeb61244a38afa8720c767ee92a4a425f
Jose Eduardo <joseduardosilva.42@gmail.com>
2021-12-14 10:54:15 -0300

    13-12

--- dtmoney/src/components/Header/index.tsx
+++ dtmoney/src/components/Header/index.tsx
@@ -1,7 +1,20 @@
+import { useState } from 'react'
 import logImg from '../../assets/Logo.svg'
+import Modal from 'react-modal';
+
 import { Container, Content } from './styles'
 
 export function Header() {
+  const [isNewTransactionOpen, setIsNewTransactionOpen] = useState(false);
+
+  function handleOpenNewTransactionOpen(){
+    setIsNewTransactionOpen(true);
+  }
+
+  function handleCloseNewTransactionOpen() {
+    setIsNewTransactionOpen(false);
+  }
+  
   return (
     <Container>
       <Content>
@@ -9,6 +22,10 @@ export function Header() {
         <button type="button">
           Nova Transação
         </button>
+
+        <Modal isOpen={isNewTransactionOpen}>
+          <h2>Cadastrar Transação</h2>
+        </Modal>
       </Content>
     </Container>
   )
