198ef91bc8f2a71013f9f203c8776913b3654135
Jose Eduardo <joseduardosilva.42@gmail.com>
2021-12-16 22:46:21 -0300

    Modal stylized

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import Modal from 'react-modal';
 
 import { GlobalStyle } from './styles/global';
+import { NewTransactionModal } from './components/NewTransactionModal';
 
 Modal.setAppElement('#root');
 export function App() {
@@ -25,12 +26,10 @@ export function App() {
 
       <Dashboard />
 
-      <Modal 
+      <NewTransactionModal 
         isOpen={isNewTransactionOpen}
-        onRequestClose ={handleCloseNewTransactionModal}
-        > 
-          <h2>Cadastrar Transação</h2>
-      </Modal>
+        onRequestClose={handleCloseNewTransactionModal}
+      />
 
       <GlobalStyle />
     </>
