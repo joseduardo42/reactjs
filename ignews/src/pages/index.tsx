946a4e0840472d1b6248df571936b93ba0235ea5
Jose Eduardo <joseduardosilva.42@gmail.com>
2021-12-21 12:19:27 -0300

    add header e home page. Initialization of API connection

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,14 +1,37 @@
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 
-export default function Home() {
+import { SubscribeButton } from '../components/SubscribeButton';
+
+import styles from './home.module.scss';
+
+export default function Home(props) {
   return (
     <>
       <Head>
-        <title>Inicio | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>
-        Hello World
-      </h1>
+      
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span>World.</h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    props: {
+      nome: 'Jose'
+    }
+  }
 }
