19a95edf4dc8c2fc75ffe7ab6d0b05c25e6e8c27
joseduardo <joseduardosilva.42@gmail.com>
2022-01-19 01:34:34 -0300

    create, stylization and add features to get posts in Prismic

--- ignews/src/pages/posts/index.tsx
+++ ignews/src/pages/posts/index.tsx
@@ -1,7 +1,22 @@
 import styles from './styles.module.scss'
 import Head from "next/head";
+import {GetStaticProps} from "next";
+import {getPrismicClient} from "../../services/prismic";
+import Prismic from "@prismicio/client";
+import { RichText } from 'prismic-dom'
+import Link from "next/link";
 
-export default function Posts() {
+type Post = {
+    slug: string;
+    title: string;
+    excerpt: string;
+    updatedAt: string;
+};
+
+interface PostsProps {
+    posts: Post[]
+}
+export default function Posts({ posts }: PostsProps) {
     return (
         <>
             <Head>
@@ -10,23 +25,44 @@ export default function Posts() {
 
             <main className={styles.container}>
                 <div className={styles.posts}>
-                    <a href="#">
-                        <time>12 de março de 2021</time>
-                        <strong>Creating a Monorepo with Lerna</strong>
-                        <p>Post here</p>
-                    </a>
-                    <a href="#">
-                        <time>12 de março de 2021</time>
-                        <strong>Creating a Monorepo with Lerna</strong>
-                        <p>Post here</p>
-                    </a>
-                    <a href="#">
-                        <time>12 de março de 2021</time>
-                        <strong>Creating a Monorepo with Lerna</strong>
-                        <p>Post here</p>
-                    </a>
+                    { posts.map(post => (
+                        <Link href={`/posts/${post.slug}`}>
+                            <a key={post.slug}>
+                                <time>{post.updatedAt}</time>
+                                <strong>{post.title}</strong>
+                                <p>{post.excerpt}</p>
+                            </a>
+                        </Link>
+                    ))}
                 </div>
             </main>
         </>
     );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    const prismic = getPrismicClient()
+    const response = await prismic.query<any>([
+        Prismic.predicates.at('document.type', 'publication')
+    ], {
+        fetch: ['publication.title', 'publication.content'],
+        pageSize: 100,
+    })
+
+    const posts = response.results.map(post => {
+        return {
+            slug: post.uid,
+            title: RichText.asText(post.data.title),
+            excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+            updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric'
+            })
+        };
+    });
+
+    return {
+        props: { posts }
+    }
 }
