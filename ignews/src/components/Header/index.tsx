43619433bf4843c902e5a0a8e5ab528af657c7a4
joseduardo <joseduardosilva.42@gmail.com>
2022-01-19 01:32:28 -0300

    stylized button when active link, alternate between post and home

--- ignews/src/components/Header/index.tsx
+++ ignews/src/components/Header/index.tsx
@@ -1,14 +1,21 @@
 import { SigInButton } from '../SigInButton';
 import styles from './styles.module.scss'
+// uso de Link para a otimização da página utilizando SPA
+import {ActiveLink} from "../ActiveLink";
 
 export function Header() {
+  //permite copiar estilização (ver no botão active)
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src={"/images/logo.svg"} alt="ig.news" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts">
+            <a> Posts</a>
+          </ActiveLink>
         </nav>
 
         <SigInButton />
