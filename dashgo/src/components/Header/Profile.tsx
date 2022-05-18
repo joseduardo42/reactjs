74cd04ddb2f9ab54518021aa363dafa303e71781
Jose <Jose>
2022-05-18 18:21:52 -0300

    responsive header

--- dashgo/src/components/Header/Profile.tsx
+++ dashgo/src/components/Header/Profile.tsx
@@ -1,16 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData: boolean
+}
+export function Profile({ showProfileData = true }: ProfileProps) {
     return (
     <Flex align="center">
-                <Box mr="4" textAlign="right">
-                    <Text>José Eduardo</Text>
-                    <Text color="gray.300" fontSize="small">
-                        joseduardo.42@gmail.com
-                    </Text>
-                </Box>
+        { showProfileData &&
+            (<Box mr="4" textAlign="right">
+            <Text>José Eduardo</Text>
+            <Text color="gray.300" fontSize="small">
+                joseduardo.42@gmail.com
+            </Text>
+        </Box>)}
 
-                <Avatar size="md" name="José Eduardo" />
-            </Flex>
+        <Avatar size="md" name="José Eduardo" />
+    </Flex>
     )
 }
